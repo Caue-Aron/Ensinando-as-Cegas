ef554f9f099b2803ca96bc6f7cb05e6034ecb5ee
Cauê <54822334+Caue-Aron@users.noreply.github.com>
2022-06-28 17:07:29 -0300

    Removido opção de mudar gênero da voz

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 return {
 -- Table: {1}
 {
+   ["fase_atual"]=1,
    ["velocidade"]=1,
    ["intro"]=1,
+   ["volume"]=1,
    ["voz_ativada"]=1,
    ["voz_feminina"]=0,
-   ["fase_atual"]=1,
-   ["volume"]=1,
 },
 }
